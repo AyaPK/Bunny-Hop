318e57c85db959859fbc373aec600af4e8c16014
AyaPK <Sherbnya@gmail.com>
2025-07-31 23:41:40 +0100

    Add spring nodes

---
+++ nodes/spring.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_area_2d_body_entered(body: Player) -> void:
+	if body in get_tree().get_nodes_in_group("player"):
+		body.velocity.y -= 600
+		animation_player.play("sprung")
+		await animation_player.animation_finished
+		animation_player.play("idle")
+	pass # Replace with function body.
