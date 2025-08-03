6ef5959a8f12ad5ea11089794eb43ce29964ba9f
AyaPK <Sherbnya@gmail.com>
2025-08-03 01:42:02 +0100

    Fix level select bug and add game export

--- menu/level_button.gd
+++ menu/level_button.gd
@@ -8,4 +8,6 @@ func _process(_delta: float) -> void:
 
 func _pressed() -> void:
 		AudioManager.menu_music.stop()
-		get_tree().change_scene_to_file("res://levels/level_"+text+".tscn")
+		var ls: String = "res://levels/level_"+text+".tscn".replace("\n", "")
+		print(ls)
+		get_tree().change_scene_to_file(ls)
